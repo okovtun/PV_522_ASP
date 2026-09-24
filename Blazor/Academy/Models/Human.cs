3374b9c49a082261d29fb15c81f63875977bbf14
User <User@DESKTOP-QHG18FL>
2026-09-24 19:17:56 +0300

    Lesson 29 - Student model DONE, TODO: Scaffold Student.

---
+++ Blazor/Academy/Models/Human.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Academy.Models
+{
+	public class Human
+	{
+		[Required]
+		[StringLength(50, MinimumLength = 2)]
+		[RegularExpression("^[A-ZА-Я][a-zа-я]*$", ErrorMessage = "Фамилия содержит недопустимые символы")]
+		public string last_name { get; set; }
+
+		[Required]
+		[StringLength(50, MinimumLength = 2)]
+		[RegularExpression("^[A-ZА-Я][a-zа-я]*$", ErrorMessage = "Имя содержит недопустимые символы")]
+		public string first_name { get; set; }
+
+		[StringLength(50, MinimumLength = 2)]
+		[RegularExpression("^[A-ZА-Я][a-zа-я]*$", ErrorMessage = "Отчество содержит недопустимые символы")]
+		public string? middle_name { get; set; }
+
+		[Required]
+		[DataType(DataType.Date)]
+		[RangeAttribute(typeof(DateOnly), "1950-01-01", "2020-01-01")]
+		DateOnly birth_date { get; set; }
+
+		public string? email { get; set; }
+		public string? phone { get; set; }
+		public byte[]? photo { get; set; }
+	}
+}
---
+++ Blazor/Academy/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Academy.Models
+{
+	public class Student:Human
+	{
+		[Key]
+		public int stud_id { get; set; }
+
+		[Required]
+		[ForeignKey(nameof(Group))]
+		public int group { get; set; }
+
+		//Navifation properties:
+		public Group Group { get; set; }
+	}
+}
